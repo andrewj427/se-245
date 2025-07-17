6bde042d086a025c1f564055f550e3b9a5a15ac0
andrewj427 <aajohnston@email.neit.edu>
2025-07-17 15:04:52 -0400

    EOD1

---
+++ week1-sample1/week1-sample1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1_sample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            //Defining variables:strings, ints, and double
+            String strFirst, strOperand, strNum1, strNum2;
+            Double intNum1 = 0, intNum2 = 0, intResult = 0;
+            Double dblResult; //double is similar to a float
+
+            //output hello to the console
+            Console.WriteLine("Hello There!");
+
+            //ask for firstname and collect data
+            Console.Write("Please enter your first name: ");
+            strFirst = Console.ReadLine();
+
+            //return first name with a message
+            Console.WriteLine("Hello, " + strFirst + "! Let's do some math!");
+
+            //collect first number
+            Console.Write("Please enter the first number: ");
+            strNum1 = Console.ReadLine();
+
+            //collect operator
+            Console.WriteLine("Please enter the math operation (PLUS, MINUS, MULTIPLY, DIVIDE): ");
+            strOperand = Console.ReadLine().ToUpper();
+
+            //collect second number
+            Console.Write("Please enter the second number: ");
+            strNum2 = Console.ReadLine();
+
+            //convert string entries into int so we can use math operations
+            intNum1 = double.Parse(strNum1);
+            //another way to convert
+            intNum2 = Convert.ToDouble(strNum2);
+
+            //type of if statement, does different actions based on the value of the string passed in the parenthesis
+            switch (strOperand)
+            {   //if strOperand == "PLUS":
+                case "PLUS":
+                    intResult = intNum1 + intNum2;
+                    break; //add the 2 numbers and leave the statement
+                //if strOperand == "MINUS":
+                case "MINUS":
+                    intResult = intNum1 - intNum2;
+                    break;
+                case "DIVIDE":
+                    intResult = intNum1 / intNum2;
+                    break;
+                case "MULTIPLY":
+                    intResult = intNum1 * intNum2;
+                    break;
+            }
+            dblResult = (Double)intResult; //cast intresult to a double
+
+            //conditional outputs based on operation chosen
+            if (strOperand == "PLUS")
+            {
+                Console.WriteLine("\n\nThe sum of " + intNum1 + " and " + intNum2 + " equals: " + dblResult);
+            }
+            else if (strOperand == "MINUS")
+            {
+                Console.WriteLine($"\n\nThe sum of {intNum1} and {intNum2} equals: {dblResult}");
+            }
+            else if (strOperand == "DIVIDE")
+            {
+                Console.WriteLine($"\n\nthe quotient of {intNum1} and {intNum2} equals: {dblResult}");
+            }
+            else if (strOperand == "MULTIPLY")
+            {
+                Console.WriteLine($"\n\nThe product of {intNum1} and {intNum2} equals: {dblResult}");
+            }
+            else
+            {
+                Console.WriteLine("Error, please try again");
+            }
+
+                Console.WriteLine("\n\nPress any key to continue"); //so program doesnt end abruptly
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week1-sample1/week1-sample1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("week1-sample1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("week1-sample1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0fcca518-4c71-460f-8fea-edaf292310a6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
